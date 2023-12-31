5b13feb1aaaecd6f47d5cc8b470614cf4a3d392c
ivainqueur <ishimvainqueur@gmail.com>
2023-12-31 22:40:22 +0200

    [feat+fix] adding swagger and environment variables to the frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,8 @@ import morgan from 'morgan'
 import authRouter from './controllers/auth.controller.js'
 import patchRouter from './controllers/patch.controller.js'
 import { config } from 'dotenv'
+import swaggerUI from 'swagger-ui-express'
+import swaggerDoc from '../swagger/swagger.json' assert { type: "json" }
 
 config()
 
@@ -16,6 +18,7 @@ const server = app.listen(3000, () => logger.info('Server running on port 3000')
 app.use(cors())
 app.use(morgan(':date[iso] :method :url :status :response-time ms - :res[content-length]'))
 app.use(express.json())
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 app.use('/api/auth', authRouter)
 app.use('/api/patch', patchRouter)
--- frontend/src/utils/axios.config.js
+++ frontend/src/utils/axios.config.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 const api = Axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         'Authorization': `Bearer ${localStorage.getItem('token')}`
     }
